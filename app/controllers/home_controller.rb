190279422acebdb57e088978c48deb5863294fec
Esther Song <esther.song62@myhunter.cuny.edu>
2017-09-30 23:17:34 -0400

    style home, login and sign up page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,16 @@ class HomeController < ApplicationController
 
   before_action :authenticate_user!, only: :authentication
   def index
+  	@header_text = "Save your peers from the struggles of school. Become a tutor."
+  	@step_one = "Need help with your studies?"
+  	@step_two = "Find a tutor on campus with Peer Tutoring"
+  	@step_three = "Learn from your own peers!"
   end
 
   def authentication
+  end
+
+  def tester
+  	render layout: 'form'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 20170926022858) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -23,6 +24,10 @@ ActiveRecord::Schema.define(version: 20170926022858) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
