fb6e9d9f17d8fc70079d6004ced8571ae2b63d30
MansurTsutiev <nyse_tm@yahoo.com>
2017-10-02 19:57:47 -0400

    add MessagesController and messages views

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,31 @@
+class MessagesController < ApplicationController
+  #before_action :logged_in_user
+  before_action :get_messages
+
+  def index
+  end
+
+  def create
+    message = current_user.messages.build(message_params)
+    if message.save
+      redirect_to messages_url
+    else
+      render 'index'
+    end
+  end
+
+
+
+
+  private
+
+    def get_messages
+      @messages = Message.for_display
+      @message  = current_user.messages.build
+    end
+
+    def message_params
+      params.require(:message).permit(:content)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+
+  get 'messages/create'
+
   root 'home#index'
 
   get 'home/authentication'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :messages   #generates all CRUD links for every action in MessagesController
 
 end
