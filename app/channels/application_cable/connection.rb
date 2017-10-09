277e94f792be1f142b62e98f8521701489dc0338
MansurTsutiev <nyse_tm@yahoo.com>
2017-10-09 14:30:22 -0400

    add login protection at ActionCable connection level and Messages Controller level

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,23 @@
 module ApplicationCable
-  class Connection < ActionCable::Connection::Base
+  class Connection < ActionCable::Connection::Base       #gets defined when a new connection is made
+
+    identified_by :current_user             #devise method
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+
+
+    private
+
+    def find_verified_user                  # this checks whether a user is authenticated with devise
+      if verified_user = env['warden'].user
+        verified_user
+      else
+        reject_unauthorized_connection      #method is supplied by Action Cable
+      end
+    end
+
   end
 end
