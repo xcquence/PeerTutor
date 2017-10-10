474ca9cc7a4df2c7e49980264585a8f63d6caf45
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-10-09 23:35:45 -0400

    add form input for subject

--- app/controllers/tutee_controller.rb
+++ app/controllers/tutee_controller.rb
@@ -1,10 +1,18 @@
 class TuteeController < ApplicationController
+
   def find_tutor
+    @subjects = Subject.all
   end
 
   def tips_management
   end
 
   def schedule
+  end
+
+  private
+
+  def tutoring_session_params
+    params.require(:tutoring_session).permit(:course, :question)
   end
 end
