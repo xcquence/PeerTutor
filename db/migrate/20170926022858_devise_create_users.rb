d641968196876b06bc2883371efb8da435e2b04a
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-10-11 19:36:16 -0400

    recreate table

--- db/migrate/20170926022858_devise_create_users.rb
+++ db/migrate/20170926022858_devise_create_users.rb
@@ -2,7 +2,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       ## Username
-      t.string :name
+      t.string :first_name
+      t.string :last_name
 
       ## Database authenticatable
       t.string :email,              null: false, default: ""
