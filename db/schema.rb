2d9b484bb7221eff156eea3c7da3587a033e8d84
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-10-10 22:44:45 -0400

    rename cloumn name to first_name in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011024106) do
+ActiveRecord::Schema.define(version: 20171011024241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20171011024106) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
