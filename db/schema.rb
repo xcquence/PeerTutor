b204dd486e9b0203978ce1644c3816e420c163d9
MansurTsutiev <nyse_tm@yahoo.com>
2017-10-11 21:25:23 -0400

    update schema.rb after running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -15,32 +15,12 @@ ActiveRecord::Schema.define(version: 20171010012309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "conversations", force: :cascade do |t|
-    t.integer "recipient_id"
-    t.integer "sender_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["recipient_id", "sender_id"], name: "index_conversations_on_recipient_id_and_sender_id", unique: true
-    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
-    t.index ["sender_id"], name: "index_conversations_on_sender_id"
-  end
-
   create_table "courses", force: :cascade do |t|
     t.integer "subject_id"
     t.string "number"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text "body"
-    t.bigint "user_id"
-    t.bigint "conversation_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "subjects", force: :cascade do |t|
@@ -83,6 +63,4 @@ ActiveRecord::Schema.define(version: 20171010012309) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "messages", "conversations"
-  add_foreign_key "messages", "users"
 end
