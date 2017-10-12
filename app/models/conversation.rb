7fe8269d06673db672257061829010a3ac9acd00
MansurTsutiev <nyse_tm@yahoo.com>
2017-10-12 00:42:42 -0400

    add method to return conversation between 2 users

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,7 +5,21 @@ class Conversation < ApplicationRecord
 
   validates :sender_id, uniqueness: { scope: :recipient_id }
 
-  def opposed_user(user)      #returns an opposed user to the requested user.
+  #returns a conversation between two requested users:
+  scope :between, -> (sender_id, recipient_id) do
+    where(sender_id: sender_id, recipient_id: recipient_id).or(
+      where(sender_id: recipient_id, recipient_id: sender_id)
+    )
+  end
+
+  def self.get(sender_id, recipient_id)
+    conversation = between(sender_id, recipient_id).first
+    return conversation if conversation.present?
+
+    create(sender_id: sender_id, recipient_id: recipient_id)
+  end
+
+  def opposed_user(user)
     user == recipient ? sender : recipient
   end
 end
