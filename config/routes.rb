27bd741b1f689c7d6b5f7f2c38ebee615e2432ba
MansurTsutiev <nyse_tm@yahoo.com>
2017-10-12 00:37:37 -0400

    add conversations resources

--- config/routes.rb
+++ config/routes.rb
@@ -32,4 +32,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :conversations, only: [:create]
+
 end
