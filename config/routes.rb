dc92c2b8a1f5f50eadada1666729a5b9aee8d479
MansurTsutiev <nyse_tm@yahoo.com>
2017-10-12 00:58:27 -0400

    route to close conversation window

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :conversations, only: [:create]
+  resources :conversations, only: [:create] do
+    member do
+      post :close
+    end
 
 end
