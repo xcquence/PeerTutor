f41e5241239241fbfcd707d70fb524eba37e53b3
MansurTsutiev <nyse_tm@yahoo.com>
2017-10-13 13:52:14 -0400

    add job to send recieved message to sender and recipient after saving that message in db

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,11 +1,44 @@
 class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  #perform will execute when perform_later method is called in message.rb
+  def perform(message)
+    sender = message.user
+    recipient = message.conversation.opposed_user(sender)
+
+    #connect with specified channel and send a conversation_id and a HTML code using a partial
+    broadcast_to_sender(sender, message)
+    broadcast_to_recipient(recipient, message)
+  end
+
+  private
+
+  def broadcast_to_sender(user, message)
+    ActionCable.server.broadcast(
+      "conversations-#{user.id}",
+      message: render_message(message, user),
+      conversation_id: message.conversation_id
+    )
+  end
+
+  def broadcast_to_recipient(user, message)
+    ActionCable.server.broadcast(
+      "conversations-#{user.id}",
+      message: render_message(message, user),
+      conversation_id: message.conversation_id
+    )
+  end
+
+
+  #in Rails 5 we can render a HTML code from any partial or a view
+  #by calling the ApplicationController.render method outside a controller
+  def render_message(message, user)
+    ApplicationController.render(
+      partial: 'messages/message',
+      locals: { message: message, user: user }
+    )
   end
 end
-
 
 
 #What is a job? A job is a class which performs code by using Redis server and is not connected with a Rails server.
