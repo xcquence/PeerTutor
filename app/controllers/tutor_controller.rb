d11e7425bbcdc9c2f8750b654d4b30afd678218c
MansurTsutiev <nyse_tm@yahoo.com>
2017-10-13 22:18:54 -0400

    add method to check if user is a tutor and redirect accordingly

--- app/controllers/tutor_controller.rb
+++ app/controllers/tutor_controller.rb
@@ -1,4 +1,13 @@
 class TutorController < ApplicationController
+
+  def index
+    #check if first time tutor
+    @user = current_user
+    unless @user.is_tutor
+      redirect_to edit_user_registration_path
+    end
+  end
+
   def incoming_requests
   end
 
