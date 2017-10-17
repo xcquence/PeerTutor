6b58cdde5c1744bfd9aae5fef9005a05180428ff
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-10-16 20:12:09 -0400

    add default value to is_tutor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012020504) do
+ActiveRecord::Schema.define(version: 20171017000733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20171012020504) do
     t.string "first_name"
     t.string "last_name"
     t.string "email", default: "", null: false
-    t.boolean "is_tutor"
+    t.boolean "is_tutor", default: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
