25855374e6e74e926059eed61961b9c2328f1e85
Mile Vojinovic <vojinovic.mile@gmail.com>
2017-10-17 18:35:05 -0400

    change tutor profile

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20171017000733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "adminpack"
 
   create_table "conversations", force: :cascade do |t|
     t.integer "recipient_id"
