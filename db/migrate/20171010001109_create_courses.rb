cc3e4df105641cda9d7269127e2cfc9d85637aad
xcQuence <johnyteen@gmail.com>
2017-10-20 17:48:13 -0400

    changed courses table

--- db/migrate/20171010001109_create_courses.rb
+++ db/migrate/20171010001109_create_courses.rb
@@ -1,11 +1,11 @@
 class CreateCourses < ActiveRecord::Migration[5.1]
   def change
-    create_table :courses do |t|
-      t.string :subject_id
+    create_table :courses, id: false do |t|
+      t.integer :subject_id
       t.string :name
-      t.integer :number
+      t.string :number
 
-      t.timestamps
+
     end
   end
 end
