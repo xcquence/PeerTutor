c234bcc90e64255575bdfd43d3d6ef69974ae58f
xcQuence <johnyteen@gmail.com>
2017-10-20 17:47:58 -0400

    changed/added subjects table

--- db/migrate/20171010000836_create_subjects.rb
+++ db/migrate/20171010000836_create_subjects.rb
@@ -1,10 +1,12 @@
 class CreateSubjects < ActiveRecord::Migration[5.1]
   def change
-    create_table :subjects do |t|
-      t.string :subject_id
+    create_table :subjects, id: false do |t|
+      ##added id
+      t.integer :id
+      t.string :abbrev
       t.string :name
 
-      t.timestamps
+
     end
   end
 end
