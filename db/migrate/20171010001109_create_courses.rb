2595e9f0bb188dbd95fdf20d3f264fb16b3867ae
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-10-21 20:53:22 -0400

    update create_courses migration file

--- db/migrate/20171010001109_create_courses.rb
+++ db/migrate/20171010001109_create_courses.rb
@@ -1,11 +1,11 @@
 class CreateCourses < ActiveRecord::Migration[5.1]
   def change
-    create_table :courses, id: false do |t|
+    create_table :courses do |t|
       t.integer :subject_id
-      t.string :name
       t.string :number
+      t.string :name
 
-
+      t.timestamps
     end
   end
 end
