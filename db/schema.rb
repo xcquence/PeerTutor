23eef4a7fc7f9f51a9f37d77e7de61f511de6560
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-10-29 00:04:37 -0400

    change subject column to user_id for tutors model

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20171017000733) do
   end
 
   create_table "tutors", force: :cascade do |t|
-    t.string "subject", default: "", null: false
+    t.integer "user_id"
     t.boolean "is_live", default: false
     t.decimal "total_tip", precision: 10, scale: 2
     t.datetime "created_at", null: false
