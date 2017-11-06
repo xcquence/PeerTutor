954a6e2417f01c826a3676d03dc4d5be9dfc721a
MansurTsutiev <nyse_tm@yahoo.com>
2017-11-05 22:31:39 -0500

    add chat conversation to tutee and tutor when request is accepted

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,9 +2,15 @@ class ChatController < ApplicationController
   def index
     session[:conversations] ||= []
 
-    @conversations = []
-    @users = User.all.where.not(id: current_user)                 #select all users that are not us
-    @conversations << Conversation.includes(:recipient, :messages)
-                                 .find_by(id: session[:conversations])
+
+    tutoring_session = TutoringSession.where(user_id: current_user.id).or(TutoringSession.where(tutor_id: current_user.id)).last
+
+
+    unless tutoring_session.nil?
+      @conversations = []
+      @users = User.all.where(id: tutoring_session.user_id).or(User.all.where(id: tutoring_session.tutor_id)).where.not(id: current_user)                 #select all users that are not us
+      @conversations << Conversation.includes(:recipient, :messages)
+                                   .find_by(id: session[:conversations])
+    end
   end
 end
