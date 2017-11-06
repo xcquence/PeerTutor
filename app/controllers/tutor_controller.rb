4c2632ec339c7cc7d9372ad2c7325ce867630d1d
MansurTsutiev <nyse_tm@yahoo.com>
2017-11-05 20:35:49 -0500

    update tutoring_session to accepted whem tutor accepts a request

--- app/controllers/tutor_controller.rb
+++ app/controllers/tutor_controller.rb
@@ -10,7 +10,12 @@ class TutorController < ApplicationController
   end
 
   def incoming_requests
-    @tutoring_sessions = TutoringSession.where(tutor_id: current_user.id)
+    @tutoring_sessions = TutoringSession.where(tutor_id: current_user.id, accepted: false)
+  end
+
+  def accept_request
+    TutoringSession.find(params[:session_id]).update(accepted: true)
+    redirect_to tutor_incoming_requests_path
   end
 
   def currently_tutoring
--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,8 @@ Rails.application.routes.draw do
 
   post 'tutor/update'
 
+  get 'tutor/accept_request'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
