53ac3970a797f81c822ac9d28db3430e66336e11
MansurTsutiev <nyse_tm@yahoo.com>
2017-11-06 22:18:42 -0500

    create conversation window only if tutoring session is accepted

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -6,7 +6,7 @@ class ChatController < ApplicationController
     tutoring_session = TutoringSession.where(user_id: current_user.id).or(TutoringSession.where(tutor_id: current_user.id)).last
 
 
-    unless tutoring_session.nil?
+    if !tutoring_session.nil? && tutoring_session.accepted
       @conversations = []
       @users = User.all.where(id: tutoring_session.user_id).or(User.all.where(id: tutoring_session.tutor_id)).where.not(id: current_user)                 #select all users that are not us
       @conversations << Conversation.includes(:recipient, :messages)
