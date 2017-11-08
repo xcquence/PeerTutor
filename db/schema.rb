b9b2a68e6eaa507559aa433fc69b029d91ef29fa
MansurTsutiev <nyse_tm@yahoo.com>
2017-11-08 13:59:13 -0500

    update shcema

--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20171106000631) do
   end
 
   create_table "tutors", force: :cascade do |t|
-    t.integer "user_id"
+    t.string "subject", default: "", null: false
     t.boolean "is_live", default: false
     t.decimal "total_tip", precision: 10, scale: 2
     t.datetime "created_at", null: false
