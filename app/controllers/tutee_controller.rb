574f8b57bd29fd8f156fd91792771567cd60b96d
MansurTsutiev <nyse_tm@yahoo.com>
2017-11-13 23:59:48 -0500

    modify method to broadcast that session was canceled to tutor

--- app/controllers/tutee_controller.rb
+++ app/controllers/tutee_controller.rb
@@ -41,7 +41,13 @@ class TuteeController < ApplicationController
   end
 
   def cancel_tutoring_session
+    @tutor = User.find(params[:tutor_id])
     TutoringSession.where(user_id: current_user.id).last.destroy!
+    ActionCable.server.broadcast(
+      "conversations-#{@tutor.id}",
+      command: "session_canceled",
+      tutee_id: current_user.id
+    )
     render 'find_tutor'
   end
 
