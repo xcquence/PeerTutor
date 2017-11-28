1f975a26f1b3fd4e3c4ceee93dd60dc7f5c597a5
MansurTsutiev <nyse_tm@yahoo.com>
2017-11-27 22:34:06 -0500

    add public ip for deployment with capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '54.147.235.220', user: 'deploy', roles: %w{web app db}
 
 # role-based syntax
 # ==================
