7dc377733e7849584f77bb568b401fef73065d7b
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-12-10 23:11:14 -0500

    added filter available tutors

--- app/controllers/tutee_controller.rb
+++ app/controllers/tutee_controller.rb
@@ -9,7 +9,7 @@ class TuteeController < ApplicationController
       @subject = Subject.new
       @command = "find_tutor"
     elsif @tutoring_session.tutor_id.nil? ## if the session created but tutee hasn't picked tutors yet
-      @tutors = User.all
+      available_tutors(@tutoring_session.course_id)
       @command = "list_of_tutors"
     elsif @tutoring_session.accepted
       @tutor = User.find(@tutoring_session.tutor_id)
@@ -32,7 +32,7 @@ class TuteeController < ApplicationController
       end
       #@courses = Course.all
     elsif @tutoring_session.tutor_id.nil? ## if the session created but tutee hasn't picked tutors yet
-      @tutors = User.all
+      available_tutors(@tutoring_session.course_id)
       respond_to do |format|
         format.js {render 'list_of_tutors'}
       end
@@ -86,6 +86,10 @@ class TuteeController < ApplicationController
   end
 
   def list_of_tutors
+    @tutoring_session = TutoringSession.where(user_id: current_user.id).last
+
+    available_tutors(@tutoring_session.course_id)
+
     # @tutors = User.all
     respond_to do |format|
       format.js {render 'list_of_tutors'}
@@ -128,6 +132,23 @@ class TuteeController < ApplicationController
       user_id: current_user.id,
       course_id: params[:course][:id].to_i
     )
+  end
+
+  def available_tutors(course_id)
+    @tutors = User.where(is_tutor: true, is_live: true).where.not(id: current_user.id).all  # @tutors.ids returns the array of live_tutor's ids
+
+    course_tutor_ids = Array.new
+    course_tutor_ids.clear
+
+    @tutors.each do |d|
+      @course_tutor = TutorCourse.where(tutor_id: d, course_id: course_id.to_i).first
+      if !@course_tutor.nil?
+        course_tutor_ids << User.find(@course_tutor.tutor_id)
+      else
+      end
+    end
+
+    @available_tutors = course_tutor_ids
   end
 
 end
