2d8c2c39b3e7141b63dd9744ff900e5ca93289b0
xcQuence <johnyteen@gmail.com>
2017-12-14 11:04:01 -0500

    placeholder piggy bank

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -5,12 +5,20 @@ class BankAccountsController < ApplicationController
 
   end
 
+  def index
+
+    
+    # tutor = Tutor.where(user_id: current_user.id )
+    # # @amount_to_deduct = params[:amount]
+    # # @amount_to_deduct = @amount_to_deduct.gsub('$', '').gsub(',', '')
+    # @current_balance = tutor.total_tip
+  end
   def create
 
-    tutor = Tutor.where(user_id: current_user.id )
-    # @amount_to_deduct = params[:amount]
-    # @amount_to_deduct = @amount_to_deduct.gsub('$', '').gsub(',', '')
-    @current_balance = tutor.total_tip
+    # tutor = Tutor.where(id: current_user.id )
+    # # @amount_to_deduct = params[:amount]
+    # # @amount_to_deduct = @amount_to_deduct.gsub('$', '').gsub(',', '')
+    # @current_balance = tutor.total_tip
 
 
 
